d8884ae1cca9c52c37d29bdd32484fa60f784ed0
Anatolij Eberle <eberle.anatol@gmail.com>
2023-12-29 10:32:41 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex justify-center flex-col w-full">
+    <img class="w-20 self-center" alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Hello Vue 3.0 + Vite" />
+    <p>hallo</p>
+  </div>
+</template>
+
+<script>
+  import HelloWorld from './components/HelloWorld.vue'
+
+  export default {
+    name: 'App',
+    components: {
+      HelloWorld
+    }
+  }
+</script>
